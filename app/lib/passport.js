d8dca03e0b12fb4a089ffe083a1370641d1c626f
Brian <choiwoosung4@gmail.com>
2019-10-18 01:28:43 +0900

    commit
    commitcommitcommitcommitcommit

--- app/lib/passport.js
+++ app/lib/passport.js
@@ -15,7 +15,7 @@ module.exports = function() {
      passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_CLIENT_ID,
         clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: 'https://urban-sketch.herokuapp.com/auth/facebook'
+        callbackURL: 'https://urban-sketch.herokuapp.com/auth/facebook/callback'
      }, 
      function (accessToken, refreshToken, profile, cb) { // 토큰 요청을 함
         return cb(null, profile);
